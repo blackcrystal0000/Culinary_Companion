eb2cda1c9b8c80553de6e749128f4a16d0d19080
Crystal <muthonigachanja2000@gmail.com>
2023-10-09 10:14:12 -0700

    Frontend and Backend changes and additions

---
+++ app/setup.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///culinary_companion.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# db.init_app(app)
+bcrypt = Bcrypt(app)
+api=Api(app)
